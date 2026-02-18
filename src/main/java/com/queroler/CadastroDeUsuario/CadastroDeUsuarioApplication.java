f14e7383fb773da0eaaf996ef815c0363edd8bfb
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2026-02-18 11:27:14 -0300

    build: Configurações iniciais.

---
+++ src/main/java/com/queroler/CadastroDeUsuario/CadastroDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.queroler.CadastroDeUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queroler/CadastroDeUsuario/CadastroDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queroler.CadastroDeUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
